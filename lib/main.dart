12e8a3ade8c2c5470a1087e4188b71dd98fcacd0
Александр Утбанов <utbanovag@it2g.ru>
2021-10-18 17:43:27 +0300

    fix orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:darkness/screens/darkness/darkness_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
   runApp(const App());
 }
 
